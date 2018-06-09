e6ad74244519fde2be09c557db4767f8e64f60c9
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-06-09 12:05:18 +0000

    test333 New files added - [rightnav,AdventureWorks2014,CountryInfoService,FeedService,MyJavaService,googleapis,heartRate,fonts,material,Readme.txt] Files modified - [Common,Login,Main,footer,header,leftnav,topnav,FileService,hrdb,securityService,WEB-INF,data,lib,momentLocale,ngLocale,pages,profiles,resources,webapp,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,README.txt,README.md] Removed files - [Accordion,AgileBoard,Article,Blog,ButtonsAndAnchor,Calendar,Cards,Carousel,Charts,Clients,Contacts,ContactsV2,CustomToaster,Dashboard,DashboardV2,DashboardV3,DashboardV4,DashboardV5,Datatables,Dialogs,EmailAttachments,EmailComposition,EmailInbox,EmailView,FAQ,FileManager,FolderListPartial,Form,FormWidget,Iframe,Invoice,IssueTracker,LN_Widgets,List,Livefilter,LoginAndSignup,LoginWithUserProfileImage,Media,Messages,MessagesPartial,Navigation,Notifications,OutlookView,Panel,PinBoard,Profile,ProfileV2,ProgressBarsAndSpinner,ProjectDetail,Projects,SignupWithSocialConnect,Signuppage,SimpleLoginWithSocialConnect,SocialFeed,SweetAlert,Tabs,TeamsBoard,Tile,Timeline,TimelinePartial,TimelineV3,Typography,UserCommentsPartial,VoteList,Widgets,Wizard,breadcrumbpartial,department,emailActionPanel,employee,feedcomments,mailOutlookView,orderDetails,pagecontent,popoverTemplate,projectMeetingScheduleDetails,projectProgressDetails,github,websocket,googlemaps,fonts,i18n,imagelists,screenshot,stock,thumbnail,vermillion,invoice.png]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,13 +1,13 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "vermillion",
+  "activeTheme" : "material",
   "dateFormat" : "",
   "defaultLanguage" : "en",
   "displayName" : "TestVCS_9june",
   "homePage" : "Main",
   "name" : "TestVCS_9june",
   "platformType" : "WEB",
-  "securityEnabled" : "false",
-  "supportedLanguages" : "ar-iq,en,es-es,fr,hi,pt,te-in",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
   "timeFormat" : "",
   "type" : "APPLICATION",
   "version" : "1.0"
