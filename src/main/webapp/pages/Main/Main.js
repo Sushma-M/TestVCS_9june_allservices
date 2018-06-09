5f3949b9102a02106c75c2e7e66fe4a8da5b079a
Sushma Maddirala <sushma.maddirala@imaginea.com>
2018-06-09 10:29:00 +0000

    rtrt New files added - [hrdb,data] Files modified - [Main,profiles,pom.xml,project-services.xml,index.html]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -15,3 +15,17 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("UserTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("UserLiveForm1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
