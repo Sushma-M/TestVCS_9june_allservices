10ffd0cbbceda09b0797289dd1a08a9140a7cf70
Sushma Maddirala <sushma.maddirala@imaginea.com>
2018-06-09 10:42:43 +0000

    e New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,en.json] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,hrdb,securityService,WEB-INF,data,lib,logos,momentLocale,ngLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestVCS_9june",
+  "homePage" : "Main",
+  "name" : "TestVCS_9june",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
